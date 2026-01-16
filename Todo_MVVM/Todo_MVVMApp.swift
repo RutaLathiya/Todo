95f31602c254e7340968340ff51b9e2b7dc1f918
Ruta <rutalathiya09@gmail.com>
2026-01-16 15:21:11 +0530

    Initial Commit

---
+++ Todo_MVVM/Todo_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_MVVMApp.swift
+//  Todo_MVVM
+//
+//  Created by iMac on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Todo_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
